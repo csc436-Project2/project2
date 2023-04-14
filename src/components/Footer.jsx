160e460a4b4023d93f912b4b52e3df3be0b9e02b
Brianj61479 <115123910+Brianj61479@users.noreply.github.com>
2023-04-13 22:49:44 -0400

    Added Header, Footer and container files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import Container from "./Container";
+
+const Footer = () => {
+  return <Container className='bg-gray-300'>Coming soon: A Footer!</Container>;
+};
+
+export default Footer;
