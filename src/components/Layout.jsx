9badea01025fe891751283ab56da12fb864b6708
Brianj61479 <115123910+Brianj61479@users.noreply.github.com>
2023-04-17 12:25:53 -0400

    updated routes data path and Layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,47 @@
+/** @format */
+import { Routes, Route, Link, Outlet, NavLink } from "react-router-dom";
+
+const Layout = () => {
+  const style = ({ isActive }) => ({
+    fontWeight: isActive ? "bold" : "normal",
+  });
+
+  return (
+    <>
+      <h1>Brian and Chris Blog Site</h1>
+
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <NavLink to='/homePage' style={style}>
+          Home
+        </NavLink>{" "}
+        |
+        <NavLink to='/createBlog' style={style}>
+          Create Blog
+        </NavLink>{" "}
+        |
+        <NavLink to='/deleteBlog' style={style}>
+          Delete Blog
+        </NavLink>{" "}
+        |
+        <NavLink to='/readBlog' style={style}>
+          Read Blog
+        </NavLink>{" "}
+        |
+        <NavLink to='/updateBlog' style={style}>
+          Update Blog
+        </NavLink>
+      </nav>
+
+      <main style={{ padding: "1rem 0" }}>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default Layout;
