500f2ba0ad0136ce28e89e0227072047e0149777
Brianj61479 <115123910+Brianj61479@users.noreply.github.com>
2023-04-18 22:14:36 -0400

    updated store imports

---
+++ store.js
@@ -0,0 +1,31 @@
+/** @format */
+
+import { configureStore } from "@reduxjs/toolkit";
+import todosReducer from "../features/todos/todosSlice";
+import filtersReducer from "../features/filters/filtersSlice";
+
+export const store = configureStore({
+  reducer: {
+    todos: todosReducer,
+    filters: filtersReducer,
+  },
+});
+
+// import {createStore} from "redux"
+
+// const initialState = {
+//     amount: "12.00",
+//     currencyCode: "USD",
+// };
+
+// function reducer(state = initialState, action){
+//     return state;
+// }
+
+// export const store = createStore(reducer);
+
+//to tell our project about store,
+//add the following to some file in the project
+
+// import { Provider } from "react-redux";
+// import {store} from "./store"
