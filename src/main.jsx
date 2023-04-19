2e324dad9335b2e0e2116cb6f64d125386b56b35
Brianj61479 <115123910+Brianj61479@users.noreply.github.com>
2023-04-18 23:02:15 -0400

    fixed configureStore import

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+// const store = configureStore();
+export const store = configureStore({
+  reducer: {
+    //todos: todosReducer,
+    // filters: filtersReducer,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
