f858e0c6edc8019babc5b3f60e0f6ee8e49c07a5
Brianj61479 <115123910+Brianj61479@users.noreply.github.com>
2023-04-19 12:25:51 -0400

    commented everything out except CRUD

---
+++ App.jsx
@@ -0,0 +1,119 @@
+/** @format */
+import "./components/Header.css";
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./components/Header.jsx";
+import CreateBlog from "./Pages/createBlog.jsx";
+import DeleteBlog from "./Pages/deleteBlog.jsx";
+import ReadBlog from "./Pages/readBlog.jsx";
+import UpdateBlog from "./Pages/updateBlog.jsx";
+import HomePage from "./Pages/homePage.jsx";
+import PageRoutes from "./components/Routes.jsx";
+import DeleteButton from "./components/deleteButton.jsx";
+import {
+  useNavigate,
+  Routes,
+  Route,
+  Link,
+  Outlet,
+  NavLink,
+  useParams,
+} from "react-router-dom";
+
+function App() {
+  return <PageRoutes />;
+}
+export default App;
+
+// const deleteRow = (id) => (e) => {
+//   console.log("id", id);
+//   console.log("Event", e);
+//   axios.delete("http://localhost:3001/v1/api/posts/${id}").then((res) => {
+//     console.log(res);
+//     console.log(res.data);
+//     const posts = data.filter((item) => item.id !== id);
+//     setData(request.data);
+//   });
+// return (
+// <button className='deleteButton' onClick={(e) => deleteRow(posts.id, e)}>
+//   Delete
+// </button>
+//);
+
+// const navigate = useNavigate();
+
+// const [loading, setLoading] = useState(false);
+// const [data, setData] = useState([]);
+// const getPosts = async () => {
+//   setLoading(true);
+//   const request = await axios.get("http://localhost:3001/v1/api/posts");
+
+//   setLoading(false);
+//   setData(request.data);
+// };
+// useEffect(() => {
+//   getPosts();
+// }, []);
+
+// if (loading) {
+//   return <p>Loading...</p>;
+// }
+// console.log(data);
+// return (
+// <div className='App'>
+// return <PageRoutes titles={titles} />;{/* <PageRoutes data={data} /> */}
+{
+  /* {data.map((post) => {
+        return (
+          <div key={post.id}>
+            Title: {post.title} | Last Updated: {post.last_updated} | Published
+            Date: {post.originally_published}
+          </div>
+        );
+      })} */
+}
+// </div>
+// );
+// }
+// const [blogTitles, setBlogTitles] = useState(titles);
+
+// const handleRemoveBlog = (titleID) => {
+//   setBlogTitles((state) => state.filter((title) => title.id !== titleID));
+
+//   navigate("/homePage");
+// };
+
+// return <PageRoutes titles={titles} onRemoveItem={handleRemoveBlog} />;
+// }
+
+// const [loading, setLoading] = useState(false);
+// const [data, setData] = useState([]);
+// const getPosts = async () => {
+//   setLoading(true);
+//   const request = await axios.get("http://localhost:3001/v1/api/posts");
+
+//   setLoading(false);
+//   setData(request.data);
+// };
+// useEffect(() => {
+//   getPosts();
+// }, []);
+
+// if (loading) {
+//   return <p>Loading...</p>;
+// }
+// console.log(data);
+// return (
+//   <div className='App'>
+//     Posts:
+//     {data.map((post) => {
+//       return <p key={post.id}>Title: {post.title}</p>;
+//     })}
+//   </div>
+// );
+
+// const Layout = () => {
+//   const style = ({ isActive }) => ({
+//     fontWeight: isActive ? "bold" : "normal",
+//   });
