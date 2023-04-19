14fc5e1d56367d2100e08f3cedb973b538553dfb
chrisupt2001 <73676459+chrisupt2001@users.noreply.github.com>
2023-04-18 23:11:30 -0400

    v1.9.7

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,9 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
 );
