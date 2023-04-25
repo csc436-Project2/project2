7804e3b5f444974ea09b424a13d6247e4e6f143f
Brianj61479 <115123910+Brianj61479@users.noreply.github.com>
2023-04-25 16:23:34 -0400

    added more test cases

--- src/Pages/CRUD.test.jsx
+++ src/Pages/CRUD.test.jsx
@@ -1,8 +1,48 @@
 /** @format */
 
 import { describe, it, expect } from "vitest";
+// import CRUD, {
+//   handleUpdate,
+//   handleChange,
+//   handleEdit,
+//   handleDelete,
+// } from "./src/store";
+// import { blogReducer } from "./src/store.js";
+// import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 
-describe("something truthy and falsy", () => {
+// import App, { getPosts } from "./src/App";
+import store, { BlogReducer } from "./store";
+
+const blogOne = {
+  title: "Blog Post #1",
+  id: "1",
+  content: "Blog Body Content #1",
+};
+const blogTwo = {
+  title: "Blog Post #2",
+  id: "2",
+  content: "Blog Body Content #2",
+};
+
+const blogs = [blogOne, blogTwo];
+
+describe("blogReducer", () => {
+  it("adds an item to the array", () => {
+    const action = { type: "create", payload: blogOne };
+    const state = { data: [], isLoading: false, isError: false };
+
+    const newState = blogReducer(state, action);
+    const expectedState = {
+      data: [blogOne],
+      isLoading: false,
+      isError: false,
+    };
+
+    expect(newState).toBe(expectedState);
+  });
+});
+
+describe("Something truthy and falsy", () => {
   it("true to be true", () => {
     expect(true).toBe(true);
   });
@@ -10,4 +50,22 @@ describe("something truthy and falsy", () => {
   it("false to be false", () => {
     expect(false).toBe(false);
   });
+});
+
+describe("handleUpdate component", () => {
+  it("updates a blog when clicking the updateButton", () => {});
+});
+
+describe("handleChange component", () => {
+  it("updates a blog when clicking the updateButton", () => {});
+});
+
+describe("handleSubmit component", () => {
+  it("updates a blog when clicking the updateButton", () => {});
+});
+describe("handleEdit component", () => {
+  it("updates a blog when clicking the updateButton", () => {});
+});
+describe("handleDelete component", () => {
+  it("updates a blog when clicking the updateButton", () => {});
 });
