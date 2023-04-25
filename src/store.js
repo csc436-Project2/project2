59bf2db41d2bdc9a7cef6d8af66d802b00c8407d
Brianj61479 <115123910+Brianj61479@users.noreply.github.com>
2023-04-25 18:10:42 -0400

    added BlogReducer as export

--- src/store.js
+++ src/store.js
@@ -1,65 +1,73 @@
-import {createStore} from "redux"
+/** @format */
+
+/** @format */
+
+import { createStore } from "redux";
 
 const initialState = {
-    data: [],
+  data: [],
 };
 
-function reducer(state = initialState, action){
-    
-    switch(action.type) {
-        case 'create':
-        case 'read':
-        case 'update':
-        case 'delete':
-          return {
-            ...state,
-            [action.type]: action.value
-          }
-        case 'create':
-          return {
-            ...state,
-            data: [...state.data, action.payload]
-          }
-        case 'read':
-          return {
-            //pass data to payload
-            ...state,
-            data: action.payload,
-          }
-        case 'update': 
-        
-        const updatedData = state.data.map(post => {
-            if(post.id === action.payload.updatedData){
-                return action.payload;
-            }
-            else
-            {
-                return post;
-            }
-        })
-        
-        return {
-            ...state,
-            data: updatedData
-          }
-        case 'delete':
-            // when firing the action, need to send id from post here
-          const filteredData = data.filter(post => action.id !== post.id);
-          return {
-            ...state,
-            data: filteredData,
-          } 
+function blogReducer(state = initialState, action) {
+  switch (action.type) {
+    case "create":
+    case "read":
+    case "update":
+    case "delete":
+      return {
+        ...state,
+        [action.type]: action.value,
       };
+    case "create":
+      return {
+        ...state,
+        data: [...state.data, action.payload],
+      };
+    case "read":
+      return {
+        //pass data to payload
+        ...state,
+        data: action.payload,
+      };
+    case "update":
+      const updatedData = state.data.map((post) => {
+        if (post.id === action.payload.updatedData) {
+          return action.payload;
+        } else {
+          return post;
+        }
+      });
 
-    return state;
+      return {
+        ...state,
+        data: updatedData,
+      };
+    case "delete":
+      // when firing the action, need to send id from post here
+      const filteredData = data.filter((post) => action.id !== post.id);
+      return {
+        ...state,
+        data: filteredData,
+      };
+  }
+
+  return state;
 }
+export const Store = createStore(blogReducer);
 
-export const Store = createStore(reducer);
-
-export {blogReducer};
-
-//to tell our project about store, 
+export { blogReducer };
+//to tell our project about store,
 //add the following to some file in the project
 
-// import { Provider } from "react-redux";
-// import {store} from "./store"
+// //import { Provider } from "react-redux";
+// //import { store } from "./store";
+// import { configureStore } from "@reduxjs/toolkit";
+// import blogsReducer from "./features/todos/blogSlice";
+// import
+// const store = configureStore({
+//   reducer: {
+//     blogs: blogsReducer,
+//   },
+// });
+
+// export default store;
